59ad48b715e032f3c9aad6ed13b79a2789d0049c
Milos Zivkovic <milos.zivkovic@tendermint.com>
2023-04-08 11:58:59 +0200

    Add basic Provider endpoints

---
+++ src/provider/provider.ts
@@ -0,0 +1,105 @@
+import { ConsensusParams, Network, Status } from './types';
+
+/**
+ * Read-only abstraction for accessing blockchain data
+ */
+export interface Provider {
+  // Account-specific methods //
+
+  /**
+   * Fetches the denomination balance of the account
+   * @param {string} address The bech32 address of the account
+   * @param {string} [denomination="ugnot"] The balance denomination
+   * @param {number} [height=0] The height for querying.
+   * If omitted, the latest height is used
+   */
+  getBalance(
+    address: string,
+    denomination?: string,
+    height?: number
+  ): Promise<number>;
+
+  /**
+   * Fetches the account sequence
+   * @param {string} address The bech32 address of the account
+   * @param {number} [height=0] The height for querying.
+   * If omitted, the latest height is used.
+   */
+  getSequence(address: string, height?: number): Promise<number>;
+
+  /**
+   * Fetches the block at the specific height, if any
+   * @param {number} height The height for querying
+   */
+  getBlock(height: number): Promise<any>; // TODO define type
+
+  /**
+   * Fetches the latest block number from the chain
+   */
+  getBlockNumber(): Promise<number>; // TODO define type
+
+  /**
+   * Fetches the block at the specific height, with transactions
+   * @param {number} height The height for querying
+   */
+  getBlockWithTransactions(height: number): Promise<any>; // TODO define type
+
+  // Network-specific methods //
+
+  /**
+   * Fetches the network information
+   */
+  getNetwork(): Promise<Network>;
+
+  /**
+   * Fetches the consensus params for the specific block height
+   * @param {number} height The height for querying
+   */
+  getConsensusParams(height: number): Promise<ConsensusParams>;
+
+  /**
+   * Fetches the current node status
+   */
+  getStatus(): Promise<Status>;
+
+  /**
+   * Fetches the current (recommended) average gas price
+   */
+  getGasPrice(): Promise<number>;
+
+  /**
+   * Estimates the gas limit for the transaction
+   * @param {any} tx The transaction that needs estimating
+   */
+  estimateGas(tx: any): Promise<number>; // TODO define type
+
+  // Transaction specific methods //
+
+  /**
+   * Sends the transaction to the node for committing.
+   * The transaction needs to be signed beforehand.
+   * @param {any} tx The signed transaction
+   */
+  sendTransaction(tx: any): Promise<any>; // TODO define type
+
+  /**
+   * Fetches the transaction using the transaction hash.
+   * Returns null if transaction has not been committed.
+   * @param {any} hash The hash of the transaction
+   */
+  getTransaction(hash: any): Promise<any>; // TODO define type
+
+  /**
+   * Fetches transaction commit information, if any.
+   * Returns null if the transaction has not been committed yet
+   * @param {any} hash The hash of the transaction
+   */
+  getTransactionCommit(hash: any): Promise<any>; // TODO define type
+
+  /**
+   * Waits for the transaction to be committed on the chain
+   * @param {any} hash The transaction hash
+   * @param {number} [timeout=15000] Optional wait timeout in MS
+   */
+  waitForTransaction(hash: any, timeout?: number): Promise<any>; // TODO define type
+}
---
+++ src/provider/types.ts
@@ -0,0 +1,109 @@
+interface NetworkInfo {
+  // flag indicating if networking is up
+  listening: boolean;
+  // IDs of the listening peers
+  listeners: string[];
+  // the number of peers (decimal)
+  n_peers: string;
+  // the IDs of connected peers
+  peers: string[];
+}
+
+export interface Network extends NetworkInfo {
+  // the chain ID of the network
+  chainID: string;
+}
+
+export interface Status {
+  // basic node information
+  node_info: NodeInfo;
+  // basic sync information
+  sync_info: SyncInfo;
+  // basic validator information
+  validator_info: ValidatorInfo;
+}
+
+interface NodeInfo {
+  // the version set of the node modules
+  version_set: VersionInfo[];
+  // validator address @ RPC endpoint
+  net_address: string;
+  // the chain ID
+  network: string;
+  software: string;
+  // version of the Tendermint node
+  version: string;
+  channels: string;
+  // user machine name
+  monkier: string;
+  other: {
+    // type of enabled tx indexing ("off" when disabled)
+    tx_index: string;
+    // the TCP address of the node
+    rpc_address: string;
+  };
+}
+
+interface VersionInfo {
+  // the name of the module
+  Name: string;
+  // the version of the module
+  Version: string;
+  // flag indicating if the module is optional
+  Optional: boolean;
+}
+
+interface SyncInfo {
+  // latest block hash
+  latest_block_hash: string;
+  // latest application hash
+  latest_app_hash: string;
+  // latest block height (decimal)
+  latest_block_height: string;
+  // latest block time in string format (YY-MM-DDTHH-mm-ss.Z)
+  latest_block_time: string;
+  // flag indicating if the node is syncing
+  catching_up: boolean;
+}
+
+interface ValidatorInfo {
+  // the address of the validator node
+  address: string;
+  // the validator's public key info
+  pub_key: PublicKey;
+  // the validator's voting power (decimal)
+  voting_power: string;
+}
+
+interface PublicKey {
+  // type of public key
+  type: string;
+  // public key value
+  value: string;
+}
+
+export interface ConsensusParams {
+  // the current block height
+  block_height: string;
+  // block consensus params
+  consensus_params: {
+    // the requested block
+    Block: {
+      // maximum tx size in bytes
+      MaxTxBytes: string;
+      // maximum data size in bytes
+      MaxDataBytes: string;
+      // maximum block size in bytes
+      MaxBlockBytes: string;
+      // block gas limit
+      MaxGas: string;
+      // block time in MS
+      TimeIotaMS: string;
+    };
+    // validator info
+    Validator: {
+      // public key information
+      PubKeyTypeURLs: string[];
+    };
+  };
+}
