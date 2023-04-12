f88ffc1f23c96bdba3abfe0b630aef4f0424d946
Milos Zivkovic <milos.zivkovic@tendermint.com>
2023-04-12 19:23:01 +0200

    Add network info fetching from WS

--- src/provider/websocket/websocket.ts
+++ src/provider/websocket/websocket.ts
@@ -1,6 +1,9 @@
 import { Provider } from '../provider';
 import { ConsensusParams, NetworkInfo, Status } from '../types';
 import { RPCRequest, RPCResponse } from '../spec/jsonrpc';
+import { newRequest } from '../spec/utility';
+import { ConsensusEndpoint } from '../endpoints';
+import { WebSocket } from 'ws';
 
 /**
  * Provider based on WS JSON-RPC HTTP requests
@@ -24,8 +27,9 @@ export class WSProvider implements Provider {
    */
   constructor(baseURL: string, requestTimeout?: number) {
     this.ws = new WebSocket(baseURL);
+
     this.ws.addEventListener('message', (event) => {
-      const response = JSON.parse(event.data) as RPCResponse<any>;
+      const response = JSON.parse(event.data as string) as RPCResponse<any>;
       const request = this.requestMap.get(response.id);
       if (request) {
         this.requestMap.delete(response.id);
@@ -40,14 +44,27 @@ export class WSProvider implements Provider {
   }
 
   /**
+   * Closes the WS connection. Required when done working
+   * with the WS provider
+   */
+  closeConnection() {
+    this.ws.close();
+  }
+
+  /**
    * Sends a request to the WS connection, and resolves
    * upon receiving the response
    * @param {RPCRequest} request the RPC request
    * @returns {Promise<RPCResponse<any>>} the RPC response
    */
-  sendRequest(request: RPCRequest): Promise<RPCResponse<any>> {
+  async sendRequest<Result>(request: RPCRequest): Promise<RPCResponse<Result>> {
+    // Make sure the connection is open
+    if (this.ws.readyState != WebSocket.OPEN) {
+      await this.waitForOpenConnection();
+    }
+
     // The promise will resolve as soon as the response is received
-    const promise = new Promise<RPCResponse<any>>((resolve, reject) => {
+    const promise = new Promise<RPCResponse<Result>>((resolve, reject) => {
       const timeout = setTimeout(() => {
         this.requestMap.delete(request.id);
 
@@ -61,6 +78,52 @@ export class WSProvider implements Provider {
 
     return promise;
   }
+
+  /**
+   * Parses the result from the response
+   * @param {RPCResponse<Result>} response the response to be parsed
+   * @returns {Result} the result of the response
+   */
+  parseResponse<Result>(response: RPCResponse<Result>): Result {
+    if (!response) {
+      throw new Error('invalid response');
+    }
+
+    if (response.error) {
+      throw new Error(response.error?.message);
+    }
+
+    if (!response.result) {
+      throw new Error('invalid response returned');
+    }
+
+    return response.result;
+  }
+
+  /**
+   * Waits for the WS connection to be established
+   * @returns {Promise<null>} resolve / reject indicating success
+   */
+  waitForOpenConnection = () => {
+    return new Promise((resolve, reject) => {
+      const maxNumberOfAttempts = 10;
+      const intervalTime = 200; //ms
+
+      let currentAttempt = 0;
+      const interval = setInterval(() => {
+        if (this.ws.readyState === WebSocket.OPEN) {
+          clearInterval(interval);
+          resolve(null);
+        }
+
+        currentAttempt++;
+        if (currentAttempt > maxNumberOfAttempts - 1) {
+          clearInterval(interval);
+          reject(new Error('Maximum number of attempts exceeded'));
+        }
+      }, intervalTime);
+    });
+  };
 
   estimateGas(tx: any): Promise<number> {
     return Promise.reject('implement me');
@@ -94,8 +157,12 @@ export class WSProvider implements Provider {
     return Promise.reject('implement me');
   }
 
-  getNetwork(): Promise<NetworkInfo> {
-    return Promise.reject('implement me');
+  async getNetwork(): Promise<NetworkInfo> {
+    const response = await this.sendRequest<NetworkInfo>(
+      newRequest(ConsensusEndpoint.NET_INFO)
+    );
+
+    return this.parseResponse<NetworkInfo>(response);
   }
 
   getSequence(address: string, height?: number): Promise<number> {
