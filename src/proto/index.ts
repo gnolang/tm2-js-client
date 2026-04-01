aa190265c5769998b6a60fd80eed48f68bea69b4
clockwork <alex@clockwork.gr>
2026-04-01 17:01:29 +0300

    fix: Proto exports

--- src/proto/index.ts
+++ src/proto/index.ts
@@ -1,4 +1,10 @@
 export {
   Any,
 } from "./google/protobuf/any.js";
+export {
+  ResponseBase, ResponseDeliverTx,
+} from "./tm2/abci.js";
+export {
+  CompactBitArray, Multisignature, PubKeyMultisig,
+} from "./tm2/multisig.js";
 export * from "./tm2/tx.js";
